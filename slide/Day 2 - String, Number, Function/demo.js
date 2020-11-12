7d543d13a1ecbec0c89e4544b8c6d3f6cb7be143
khiemkino <khiemkino@gmail.com>
2020-11-12 14:23:11 +0700

    Update book slide and assigment

---
+++ slide/Day 2 - String, Number, Function/demo.js
@@ -0,0 +1,53 @@
+// Variable Type Number
+123.00
+Number()
+parseInt()
+parseFloat()
+
+'*'
+'/'
+'+'
+'-'
+'e'
+'%'
+'++'
+'--'
+'1e' = 10
+
+const number = '10'
+console.log(
+  'Number to string: ',
+  number.toString(),
+  number.toString(16),
+  number.toString(2)
+)
+
+// Name, params
+function functionA() {
+  // Statement
+}
+
+// Declare variable
+const functionB = (a, b) => {
+  return a, b
+}
+
+const functionC = (a = 'default') => {
+
+}
+
+const funcD = () => '123'
+
+// Some function in String
+length
+slice
+substring
+indexOf
+search
+toUpperCase
+toLowerCase
+replace
+trim
+concat
+split
+join
